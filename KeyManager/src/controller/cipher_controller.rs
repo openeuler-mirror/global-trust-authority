167ca5272a33f735a50b724b75aa438c8ed7407e
ryk <renjunkai1@h-partners.com>
2025-04-22 15:41:58 +0800

    修改url

--- KeyManager/src/controller/cipher_controller.rs
+++ KeyManager/src/controller/cipher_controller.rs
@@ -2,7 +2,7 @@ use actix_web::{get, HttpResponse, ResponseError};
 use crate::key_manager::secret_manager_factory::SecretManagerFactory;
 use crate::key_manager::secret_manager_factory::SecretManagerType::OpenBao;
 
-#[get("/ciphers")]
+#[get("/get_key")]
 pub async fn get_ciphers() -> HttpResponse {
     match SecretManagerFactory::create_manager(OpenBao).get_all_secret().await {
         Ok(ciphers) => HttpResponse::Ok().json(ciphers),
