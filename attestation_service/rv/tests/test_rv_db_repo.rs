9a14d661edb5db6ad27d33a624c5418e6254cc0b
heyouliang <heyouliang3@huawei.com>
2025-05-26 16:12:43 +0800

    fix:modify issue  and  add Copyright

--- attestation_service/rv/tests/test_rv_db_repo.rs
+++ attestation_service/rv/tests/test_rv_db_repo.rs
@@ -1,3 +1,15 @@
+/*
+ * Copyright (c) Huawei Technologies Co., Ltd. 2025. All rights reserved.
+ * Global Trust Authority is licensed under the Mulan PSL v2.
+ * You can use this software according to the terms and conditions of the Mulan PSL v2.
+ * You may obtain a copy of Mulan PSL v2 at:
+ *     http://license.coscl.org.cn/MulanPSL2
+ * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND, EITHER EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT, MERCHANTABILITY OR FIT FOR A PARTICULAR
+ * PURPOSE.
+ * See the Mulan PSL v2 for more details.
+ */
+
 use sea_orm::{DatabaseBackend, MockDatabase, MockExecResult, Set};
 use rv::entities::db_model::rv_db_model::ActiveModel;
 use rv::entities::db_model::rv_detail_db_model::Model;
@@ -258,7 +270,7 @@ async fn test_del_by_type_not_found() {
 
 #[tokio::test]
 async fn test_del_by_type_special_chars() {
-    // test data - 包含特殊字符
+    // test data 
     let user_id = "test@user";
     let attester_type = "TPM-2.0";
 
