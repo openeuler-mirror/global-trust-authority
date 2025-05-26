b59e5d788700e2fe9908cc0aa1016b9e2d0889c4
lyz <lvyingze@huawei.com>
2025-05-26 02:34:18 +0000

    add copyright information at the beginning of the code files

--- attestation_service/endorserment/tests/test_cert_repository.rs
+++ attestation_service/endorserment/tests/test_cert_repository.rs
@@ -1,3 +1,15 @@
+/*
+ * Copyright (c) Huawei Technologies Co., Ltd. 2025. All rights reserved.
+ * Global Trust Authority is licensed under the Mulan PSL v2.
+ * You can use this software according to the terms and conditions of the Mulan PSL v2.
+ * You may obtain a copy of Mulan PSL v2 at:
+ *     http://license.coscl.org.cn/MulanPSL2
+ * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND, EITHER EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT, MERCHANTABILITY OR FIT FOR A PARTICULAR
+ * PURPOSE.
+ * See the Mulan PSL v2 for more details.
+ */
+
 use std::path::{Path, PathBuf};
 use cert_info::ActiveModel;
 use endorserment::entities::cert_revoked_list::Model as CertRevokedListModel;
