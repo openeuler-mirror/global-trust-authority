265a211dd9c4c7fa7dfcfdb69d5c15fd853a0487
songdongfang <songdongfang@huawei.com>
2025-05-29 11:43:37 +0800

    fix：Fix bug in key management to obtain the highest key version

--- attestation_service/key_management/src/key_manager/cache/store.rs
+++ attestation_service/key_management/src/key_manager/cache/store.rs
@@ -1,33 +1,37 @@
 /*
  * Copyright (c) Huawei Technologies Co., Ltd. 2025. All rights reserved.
  * Global Trust Authority is licensed under the Mulan PSL v2.
- * You can use this software according to the terms and conditions of the Mulan PSL v2.
- * You may obtain a copy of Mulan PSL v2 at:
+ * You can use this software according to the terms and conditions of the
+ * Mulan PSL v2. You may obtain a copy of Mulan PSL v2 at:
  *     http://license.coscl.org.cn/MulanPSL2
- * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND, EITHER EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT, MERCHANTABILITY OR FIT FOR A PARTICULAR
- * PURPOSE.
+ * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY
+ * KIND, EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO
+ * NON-INFRINGEMENT, MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
  * See the Mulan PSL v2 for more details.
  */
 
+use std::collections::HashMap;
+use std::hash::Hash;
+use std::sync::{Arc, RwLock};
+
+use once_cell::sync::OnceCell;
+
 use crate::key_manager::cache::entity::key_pair::KeyPair;
 use crate::key_manager::error::KeyManagerError;
-use once_cell::sync::OnceCell;
-use std::collections::HashMap;
-use std::sync::Arc;
-use std::sync::RwLock;
 
 type VersionMap = Arc<RwLock<HashMap<String, KeyPair>>>;
 
 #[derive(Debug)]
 pub struct KeyStore {
     pub(crate) inner: HashMap<String, VersionMap>,
-    pub(crate) latest_version: OnceCell<String>,
+    pub(crate) latest_versions: HashMap<String, OnceCell<String>>,
 }
 
 // implementing thread safety tags
-unsafe impl Send for KeyStore {}
-unsafe impl Sync for KeyStore {}
+unsafe impl Send for KeyStore {
+}
+unsafe impl Sync for KeyStore {
+}
 
 impl KeyStore {
     pub fn global() -> &'static Self {
@@ -37,23 +41,17 @@ impl KeyStore {
             for key_type in ["FSK", "NSK", "TSK"] {
                 map.insert(key_type.to_string(), Arc::new(RwLock::new(HashMap::new())));
             }
-            Self {
-                inner: map,
-                latest_version: OnceCell::new(),
-            }
+            let latest_versions = HashMap::from([
+                ("FSK".to_string(), OnceCell::new()),
+                ("NSK".to_string(), OnceCell::new()),
+                ("TSK".to_string(), OnceCell::new()),
+            ]);
+            Self { inner: map, latest_versions }
         })
     }
 
-    pub fn insert(
-        &self,
-        key_type: &str,
-        version: &str,
-        key_pair: KeyPair,
-    ) -> Result<(), KeyManagerError> {
-        let versions = self
-            .inner
-            .get(key_type)
-            .ok_or(KeyManagerError::new("Key type not found"))?;
+    pub fn insert(&self, key_type: &str, version: &str, key_pair: KeyPair) -> Result<(), KeyManagerError> {
+        let versions = self.inner.get(key_type).ok_or(KeyManagerError::new("Key type not found"))?;
 
         let mut versions = versions.write().unwrap();
         if versions.contains_key(version) {
@@ -81,17 +79,16 @@ impl KeyStore {
             .collect()
     }
 
-    pub fn get_latest_version(&self) -> Option<&str> {
-        let latest_version = self.latest_version.get_or_init(|| {
-            self.list_versions()
-                .into_iter()
-                .filter_map(|v| {
-                    v.strip_prefix('v')
-                        .and_then(|s| s.parse::<u32>().ok())
-                        .map(|n| (n, v))
-                })
+    pub fn get_latest_version(&self, key_type: &str) -> Option<&str> {
+        let latest_version = self.latest_versions.get(key_type)?.get_or_init(|| {
+            let versions = self.inner.get(key_type).unwrap();
+            let versions = versions.read().unwrap();
+
+            versions
+                .keys()
+                .filter_map(|v| v.strip_prefix('v').and_then(|s| s.parse::<u32>().ok()).map(|n| (n, v.as_str())))
                 .max_by_key(|&(n, _)| n)
-                .map(|(_, v)| v)
+                .map(|(_, v)| v.to_string())
                 .unwrap_or_default()
         });
         Some(latest_version)
@@ -102,19 +99,26 @@ impl KeyStore {
 mod tests {
 
     use openssl::rsa::Rsa;
+
     use super::*;
 
     #[test]
     fn key_store() {
         let store = KeyStore::global();
         let rsa = Rsa::generate(2048).unwrap();
-        store.insert("FSK", "v1", KeyPair {
-            cached_private: OnceCell::new(),
-            cached_public: OnceCell::new(),
-            private_bytes: rsa.private_key_to_pem().unwrap(),
-            public_bytes: rsa.public_key_to_pem().unwrap(),
-            algorithm: "RS256".to_string(),
-        }).unwrap();
+        store
+            .insert(
+                "FSK",
+                "v1",
+                KeyPair {
+                    cached_private: OnceCell::new(),
+                    cached_public: OnceCell::new(),
+                    private_bytes: rsa.private_key_to_pem().unwrap(),
+                    public_bytes: rsa.public_key_to_pem().unwrap(),
+                    algorithm: "RS256".to_string(),
+                },
+            )
+            .unwrap();
         let result = store.get("FSK", "v1").unwrap();
         assert_eq!(result.public_key().size(), 256);
     }
--- attestation_service/key_management/src/key_manager/lifecycle/key_subject.rs
+++ attestation_service/key_management/src/key_manager/lifecycle/key_subject.rs
@@ -1,46 +1,43 @@
 /*
  * Copyright (c) Huawei Technologies Co., Ltd. 2025. All rights reserved.
  * Global Trust Authority is licensed under the Mulan PSL v2.
- * You can use this software according to the terms and conditions of the Mulan PSL v2.
- * You may obtain a copy of Mulan PSL v2 at:
+ * You can use this software according to the terms and conditions of the
+ * Mulan PSL v2. You may obtain a copy of Mulan PSL v2 at:
  *     http://license.coscl.org.cn/MulanPSL2
- * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND, EITHER EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT, MERCHANTABILITY OR FIT FOR A PARTICULAR
- * PURPOSE.
+ * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY
+ * KIND, EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO
+ * NON-INFRINGEMENT, MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
  * See the Mulan PSL v2 for more details.
  */
 
+use std::sync::Arc;
+use std::thread;
+use std::time::Duration;
+
+use common_log::{error, info};
+use distributed_lock::Lock as DistributedLock;
+use mockall::automock;
+use rdb::get_connection;
+use sea_orm::TransactionTrait;
+
+use crate::config::{ConfigLoader, YamlConfigLoader};
 use crate::key_manager::algorithm::create_algorithm;
 use crate::key_manager::cache::store::KeyStore;
 use crate::key_manager::error::KeyManagerError;
 use crate::key_manager::lifecycle::key_observer::observer_init::register::OBSERVER_REGISTRY;
 use crate::key_manager::lifecycle::key_observer::KeyLifecycleObserver;
-use crate::key_manager::model::KeyVersionModel;
-use crate::key_manager::model::{VaultResponse, Version};
-use crate::key_manager::orm::entity::key_manager_key_version::get_current_key_version;
-use crate::key_manager::orm::entity::key_manager_key_version::update_key_version;
-use distributed_lock::Lock as DistributedLock;
-use sea_orm::TransactionTrait;
-use std::sync::Arc;
-use std::thread;
-use std::time::Duration;
-use mockall::automock;
-use common_log::{error, info};
-use rdb::get_connection;
-use crate::config::{ConfigLoader, YamlConfigLoader};
+use crate::key_manager::model::{KeyVersionModel, VaultResponse, Version};
+use crate::key_manager::orm::entity::key_manager_key_version::{get_current_key_version, update_key_version};
 
 #[derive(Debug)]
 pub struct KeySubject {
     observers: Vec<Arc<dyn KeyLifecycleObserver + Send + Sync>>,
-    lock: KeyRotationLock
+    lock: KeyRotationLock,
 }
 
 impl KeySubject {
     pub fn new() -> Self {
-        KeySubject {
-            observers: Vec::new(),
-            lock: KeyRotationLock::new()
-        }
+        KeySubject { observers: Vec::new(), lock: KeyRotationLock::new() }
     }
 
     // Add watchers
@@ -49,10 +46,7 @@ impl KeySubject {
     }
 
     // Inform all observers
-    async fn notify_observers(
-        &self,
-        key_version: &str,
-    ) -> Result<(), KeyManagerError> {
+    async fn notify_observers(&self, key_version: &str) -> Result<(), KeyManagerError> {
         let registry = OBSERVER_REGISTRY.get().unwrap();
         let observers = registry.lock().unwrap();
         let db_connection = get_connection().await.unwrap();
@@ -68,17 +62,17 @@ impl KeySubject {
                 Err(_e) => {
                     error!("KeySubject: Failed to notify observers");
                     // Roll back the transaction
-                    if let Some(tx_arc) =  tx_holder.take() {
+                    if let Some(tx_arc) = tx_holder.take() {
                         match Arc::try_unwrap(tx_arc) {
                             Ok(_tx) => (),
                             Err(_e) => {
-                                return Err(KeyManagerError::new("Unable to rollback the transaction: there are unreleased references.".to_string()));
-                            }
+                                return Err(KeyManagerError::new(
+                                    "Unable to rollback the transaction: there are unreleased references.".to_string(),
+                                ));
+                            },
                         }
                     }
-                    return Err(KeyManagerError::new(
-                        "Failed to notify observers".to_string(),
-                    ));
+                    return Err(KeyManagerError::new("Failed to notify observers".to_string()));
                 },
             }
         }
@@ -86,7 +80,11 @@ impl KeySubject {
         if let Some(tx_arc) = tx_holder.take() {
             match Arc::try_unwrap(tx_arc) {
                 Ok(tx) => tx.commit().await?,
-                Err(_) => return Err(KeyManagerError::new("Unable to submit the transaction: there are unreleased references.".to_string())),
+                Err(_) => {
+                    return Err(KeyManagerError::new(
+                        "Unable to submit the transaction: there are unreleased references.".to_string(),
+                    ))
+                },
             }
         }
         info!("KeySubject: Notified observers");
@@ -133,21 +131,19 @@ impl KeyLifecycle for KeySubject {
                 Version::new("v1")
             });
             info!("Database key version: {}", db_version);
-            // Derived key database version v1: Query the latest key v5: Derived key range i v1-v5
+            // Derived key database version v1: Query the latest key v5: Derived key range i
+            // v1-v5
             Self::generate_key_pair(&vault_response, &db_version).await?;
 
             let max_version = vault_response.find_max_version().unwrap();
             info!("Max version: {}", max_version);
             if db_version > max_version {
-                info!(
-                    "db_version is large than max_version, db_version: {}, max_version: {}",
-                    db_version, max_version
-                );
+                info!("db_version is large than max_version, db_version: {}, max_version: {}", db_version, max_version);
                 panic!("The key data version is incorrect, and the service failed to start");
             }
             let key_store = KeyStore::global();
-            let latest_version = key_store.get_latest_version().unwrap();
-            info!("KeySubject: latest_version: {}", latest_version);
+            let latest_version = key_store.get_latest_version("TSK").unwrap();
+            println!("KeySubject: latest_version: {}", latest_version);
             // Get distributed locks
             self.lock.acquire()?;
             info!("KeySubject: Acquired key rotation lock");
@@ -167,9 +163,7 @@ impl KeyLifecycle for KeySubject {
                 if notify_result.await.is_err() {
                     error!("KeySubject: Failed to notify observers");
                     // Roll back the transaction
-                    return Err(KeyManagerError::new(
-                        "Failed to notify observers".to_string(),
-                    ));
+                    return Err(KeyManagerError::new("Failed to notify observers".to_string()));
                 }
                 info!("max_version.to_string(): {}", max_version.to_string());
                 // Update the database key version
@@ -191,16 +185,15 @@ impl KeyLifecycle for KeySubject {
         async move {
             info!("KeySubject: Generating key pair...");
             let store = KeyStore::global();
-            let process =
-                |(key_type, keys): (&str, &Vec<KeyVersionModel>)| -> Result<(), KeyManagerError> {
-                    let keys = keys.iter().filter(|v| &v.version >= db_version);
+            let process = |(key_type, keys): (&str, &Vec<KeyVersionModel>)| -> Result<(), KeyManagerError> {
+                let keys = keys.iter().filter(|v| &v.version >= db_version);
 
-                    keys.for_each(|v| {
-                        init_store(&store, key_type, v).unwrap();
-                        info!("KeySubject: Initialized store for key type: {}", key_type);
-                    });
-                    Ok(())
-                };
+                keys.for_each(|v| {
+                    init_store(&store, key_type, v).unwrap();
+                    info!("KeySubject: Initialized store for key type: {}", key_type);
+                });
+                Ok(())
+            };
             let key_vec = || {
                 let yaml = YamlConfigLoader;
                 let config = yaml.load_config().expect("can't load YamlConfig");
@@ -214,30 +207,19 @@ impl KeyLifecycle for KeySubject {
 
                 keys
             };
-            key_vec()
-                .into_iter()
-                .try_for_each(process)
+            key_vec().into_iter().try_for_each(process)
         }
     }
 }
 
-fn init_store(
-    store: &KeyStore,
-    key_type: &str,
-    v: &KeyVersionModel,
-) -> Result<(), KeyManagerError> {
+fn init_store(store: &KeyStore, key_type: &str, v: &KeyVersionModel) -> Result<(), KeyManagerError> {
     info!("KeySubject: Initializing store for key type: {}", key_type);
     let key_pair = create_algorithm(&v.algorithm).and_then(|alg| {
-        alg.derive_public(&v.private_key)
-            .or_else(|_| {
-                panic!("Failed to derive public key for key type: {}", key_type);
-            })
+        alg.derive_public(&v.private_key).or_else(|_| {
+            panic!("Failed to derive public key for key type: {}", key_type);
+        })
     })?;
-    store.insert(
-        key_type,
-        &v.version,
-        key_pair
-    )
+    store.insert(key_type, &v.version, key_pair)
 }
 
 #[derive(Debug)]
@@ -247,9 +229,7 @@ struct KeyRotationLock {
 
 impl KeyRotationLock {
     pub fn new() -> Self {
-        Self {
-            lock: DistributedLock::new("key_rotation_lock", "key_rotation_lock", 30, 1).unwrap(),
-        }
+        Self { lock: DistributedLock::new("key_rotation_lock", "key_rotation_lock", 30, 1).unwrap() }
     }
 
     pub fn acquire(&self) -> Result<(), KeyManagerError> {
@@ -258,16 +238,16 @@ impl KeyRotationLock {
                 Ok(true) => {
                     info!("KeySubject: Acquired key rotation lock");
                     return Ok(());
-                }
+                },
                 Ok(false) => {
                     info!("Failed to acquire key rotation lock, retrying...");
                     thread::sleep(Duration::from_secs(5));
                     continue;
-                }
+                },
                 Err(e) => {
                     error!("Failed to acquire key rotation lock: {}", e);
                     return Err(KeyManagerError::new(e.to_string()));
-                }
+                },
             }
         }
     }
